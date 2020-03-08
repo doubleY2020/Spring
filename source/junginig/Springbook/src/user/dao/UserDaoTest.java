c8b8508a1327071f494f8c01dcf09ada5bca2931
jungining <jungin641@gmail.com>
2020-03-08 14:59:55 +0900

    1.4 제어의 역전

--- source/junginig/Springbook/src/user/dao/UserDaoTest.java
+++ source/junginig/Springbook/src/user/dao/UserDaoTest.java
@@ -7,16 +7,10 @@ import user.domain.User;
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-		// UserDao가 사용할 ConnecionMaker 구현 클래스를 결정하고, 오브젝트를 만든다
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-
-		// 1. UserDao 생성
-        // 2. 사용할 ConnectionMaker타입의 오브젝트 제공
-        // 결국 두 오브젝트 사이의 의존관계 설정 효과
-		UserDao dao = new UserDao(connectionMaker);
+		UserDao dao = new DaoFactory().userDao();
 
 		User user = new User();
-		user.setId("hi");
+		user.setId("h2i");
 		user.setName("hh");
 		user.setPassword("hhhh");
 
