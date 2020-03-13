f36c9afd07f8e7aa5cec4fa9fa4c11fce4362dc7
jungining <jungin641@gmail.com>
2020-03-14 00:56:02 +0900

    1.6 싱글톤 레지스트리와 오브젝트 스코프

--- source/junginig/Springbook/src/user/dao/UserDaoTest.java
+++ source/junginig/Springbook/src/user/dao/UserDaoTest.java
@@ -10,21 +10,10 @@ public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		UserDao dao = context.getBean("userDao", UserDao.class);
+		UserDao dao3 = context.getBean("userDao", UserDao.class);
+		UserDao dao4 = context.getBean("userDao", UserDao.class);
 
-		User user = new User();
-		user.setId("h20i");
-		user.setName("hh");
-		user.setPassword("hhhh");
-
-		dao.add(user);
-
-        System.out.println(user.getId() + " 등록 성공");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-
-        System.out.println(user2.getId() + " 조회 성공");
+        System.out.println(dao3); //user.dao.UserDao@48f278eb
+        System.out.println(dao4); //user.dao.UserDao@48f278eb
 	}
 }
