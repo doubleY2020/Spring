c48a9c248874b7d2cba4157e5038114aacca1d99
yunmiko <kyuuuunmi@gmail.com>
2020-03-15 18:11:56 +0900

    1.5 스프링의 IoC

--- source/yuuuunmi/src/springbook/user/domain/UserDaoTest.java
+++ source/yuuuunmi/src/springbook/user/domain/UserDaoTest.java
@@ -1,10 +1,16 @@
 package springbook.user.domain;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("dbsal");
