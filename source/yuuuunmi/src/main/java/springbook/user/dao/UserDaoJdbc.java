117ab72a497a71b2a653d5ff949b1cf1a5a21f76
yunmiko <kyuuuunmi@gmail.com>
2020-05-24 15:19:52 +0900

    5.1.2

--- source/yuuuunmi/src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ source/yuuuunmi/src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -2,6 +2,7 @@ package springbook.user.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
@@ -20,11 +21,16 @@ public class UserDaoJdbc implements UserDao {
         user.setId(rs.getString("id"));
         user.setName(rs.getString("name"));
         user.setPassword(rs.getString("password"));
+        user.setLevel(Level.valueOf(rs.getInt("level")));
+        user.setLogin(rs.getInt("login"));
+        user.setRecommend(rs.getInt("recommend"));
         return user;
     };
 
     public void add(final User user) {
-        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
+        this.jdbcTemplate.update("insert into users(id, name, password, level, login, recommend) " +
+                        "values(?,?,?,?,?,?)",
+                user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend());
 
     }
 
@@ -40,6 +46,14 @@ public class UserDaoJdbc implements UserDao {
 
     public int getCount() {
         return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+    }
+
+    @Override
+    public void update(User user1) {
+        this.jdbcTemplate.update(
+                "update users set name = ? , password = ?, level = ? , login = ? , recommend = ? "
+                        + "where id = ?", user1.getName(), user1.getPassword(), user1.getLevel().intValue(), user1.getLogin(), user1.getRecommend(), user1.getId()
+        );
     }
 
     public List<User> getAll() {
--- source/yuuuunmi/src/main/java/springbook/user/domain/User.java
+++ source/yuuuunmi/src/main/java/springbook/user/domain/User.java
@@ -4,6 +4,33 @@ public class User {
     String id;
     String name;
     String password;
+    Level level;
+    int login; // 로그인 횟수
+    int recommend;  // 추천수
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public int getLogin() {
+        return login;
+    }
+
+    public void setLogin(int login) {
+        this.login = login;
+    }
+
+    public int getRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(int recommend) {
+        this.recommend = recommend;
+    }
 
     public String getId() {
         return id;
@@ -30,6 +57,15 @@ public class User {
     }
 
     public User() {
+    }
+
+    public User(String id, String name, String password, Level level, int login, int recommend) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.level = level;
+        this.login = login;
+        this.recommend = recommend;
     }
 
     public User(String id, String name, String password) {
