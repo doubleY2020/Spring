8e8dedeb9f3ebea76cc7e5446f12793452f98745
yunmiko <kyuuuunmi@gmail.com>
2020-12-20 19:22:37 +0900

    6.3.4 다이내믹 프록시를 위한 팩토리 빈 - 트랜잭션 프록시 팩토리 빈 선언

--- source/yuuuunmi/src/test/java/springbook/user/service/UserServiceTest.java
+++ source/yuuuunmi/src/test/java/springbook/user/service/UserServiceTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -15,7 +17,6 @@ import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
-import java.lang.reflect.Proxy;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -35,7 +36,6 @@ public class UserServiceTest {
     @Autowired
     UserServiceImpl userServiceImpl;
 
-
     @Autowired
     UserDao userDao;
 
@@ -49,6 +49,9 @@ public class UserServiceTest {
     MailSender mailSender;
 
     List<User> users;
+
+    @Autowired
+    ApplicationContext context;
 
     @Before
     public void setUp() {
@@ -120,20 +123,16 @@ public class UserServiceTest {
     }
 
     @Test
-    public void upgradeAllOrNothing() {
+    @DirtiesContext
+    public void upgradeAllOrNothing() throws Exception {
         UserServiceImpl testUserService = new TestUserService(users.get(3).getId());
         testUserService.setUserDao(this.userDao);
         testUserService.setMailSender(mailSender);
 
-        TransactionHandler txHandler = new TransactionHandler();
-        txHandler.setTarget(testUserService);
-        txHandler.setTransactionManager(transactionManager);
-        txHandler.setPattern("upgradeLevels");
+        TxProxyFactoryBean txProxyFactoryBean = context.getBean("&userService", TxProxyFactoryBean.class);
+        txProxyFactoryBean.setTarget(testUserService);
 
-        UserService txUserService = (UserService) Proxy.newProxyInstance(
-                getClass().getClassLoader(),
-                new Class[]{UserService.class}, txHandler
-        );
+        UserService txUserService = (UserService) txProxyFactoryBean.getObject();
 
         userDao.deleteAll();
         for (User user : users) userDao.add(user);
