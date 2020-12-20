ed215202262d13312ad3a57f1f25602e3fb46e51
yunmiko <kyuuuunmi@gmail.com>
2020-12-20 15:43:59 +0900

    6.1.1 메소드 분리 - 비즈니스 로직 코드를 트랜잭션 코드와 분리하였다.

--- source/yuuuunmi/src/main/java/springbook/user/service/UserService.java
+++ source/yuuuunmi/src/main/java/springbook/user/service/UserService.java
@@ -36,18 +36,23 @@ public class UserService {
         TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
 
         try {
-            List<User> users = userDao.getAll();
-            for (User user : users) {
-
-                if (canUpgradeLevel(user)) {
-                    upgradeLevel(user);
-                }
-            }
+            upgradeLevelsInternal();
             this.transactionManager.commit(status);
         } catch (RuntimeException e) {
             this.transactionManager.rollback(status);
             throw e;
         }
+    }
+
+    private void upgradeLevelsInternal() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
+            }
+        }
+
     }
 
     protected void upgradeLevel(User user) {
